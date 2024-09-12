47b8d432b24f1e2adc809f196c1c5aeb3e8979ef
Mateusz Zając <matzayonc@gmail.com>
2024-09-12 19:14:56 +0200

    prepare for tests

--- tests/escrow.ts
+++ tests/escrow.ts
@@ -1,43 +1,157 @@
 import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
 import { Escrow } from "../target/types/escrow";
-import { PublicKey, Keypair } from "@solana/web3.js";
+import { makeKeypairs } from "@solana-developers/helpers";
 import { assert } from "chai";
+import {
+  MINT_SIZE,
+  TOKEN_2022_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountIdempotentInstruction,
+  createInitializeMint2Instruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMinimumBalanceForRentExemptMint,
+} from "@solana/spl-token";
+import {
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Keypair,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
 
 describe("escrow", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Escrow as Program<Escrow>;
-  const maker = new Keypair();
-  const taker = new Keypair();
+  const [alice, bob, tokenMintA, tokenMintB] = makeKeypairs(4);
+
   const offerId = new BN(0);
   const offerAddress = PublicKey.findProgramAddressSync(
     [
       Buffer.from("offer"),
-      maker.publicKey.toBuffer(),
+      alice.publicKey.toBuffer(),
       offerId.toArrayLike(Buffer, "le", 8),
     ],
     program.programId
   )[0];
 
-  before(async () => {
-    for (let keypair of [maker, taker]) {
-      await program.provider.connection.requestAirdrop(
-        keypair.publicKey,
-        1000000000
+  before(
+    "Creates Alice and Bob accounts, 2 token mints, and associated token accounts for both tokens for both users",
+    async () => {
+      const [
+        aliceTokenAccountA,
+        aliceTokenAccountB,
+        bobTokenAccountA,
+        bobTokenAccountB,
+      ] = [alice, bob]
+        .map((keypair) =>
+          [tokenMintA, tokenMintB].map((mint) =>
+            getAssociatedTokenAddressSync(
+              mint.publicKey,
+              keypair.publicKey,
+              false,
+              TOKEN_PROGRAM_ID
+            )
+          )
+        )
+        .flat();
+
+      // Airdrops to users, and creates two tokens mints 'A' and 'B'"
+      let minimumLamports = await getMinimumBalanceForRentExemptMint(
+        program.provider.connection
       );
-      await program.provider.connection.requestAirdrop(
-        keypair.publicKey,
-        1000000000
+
+      const sendSolInstructions: Array<TransactionInstruction> = [
+        alice,
+        bob,
+      ].map((account) =>
+        SystemProgram.transfer({
+          fromPubkey: program.provider.publicKey,
+          toPubkey: account.publicKey,
+          lamports: 10 * LAMPORTS_PER_SOL,
+        })
       );
-      await sleep(1000);
-      const balance = await program.provider.connection.getBalance(
-        keypair.publicKey
+
+      const createMintInstructions: Array<TransactionInstruction> = [
+        tokenMintA,
+        tokenMintB,
+      ].map((mint) =>
+        SystemProgram.createAccount({
+          fromPubkey: program.provider.publicKey,
+          newAccountPubkey: mint.publicKey,
+          lamports: minimumLamports,
+          space: MINT_SIZE,
+          programId: TOKEN_PROGRAM_ID,
+        })
       );
-      assert.equal(balance, 1000000000);
+
+      // Make tokenA and tokenB mints, mint tokens and create ATAs
+      const mintTokensInstructions: Array<TransactionInstruction> = [
+        {
+          mint: tokenMintA.publicKey,
+          authority: alice.publicKey,
+          ata: aliceTokenAccountA,
+        },
+        {
+          mint: tokenMintB.publicKey,
+          authority: bob.publicKey,
+          ata: bobTokenAccountB,
+        },
+      ].flatMap((mintDetails) => [
+        createInitializeMint2Instruction(
+          mintDetails.mint,
+          6,
+          mintDetails.authority,
+          null,
+          TOKEN_PROGRAM_ID
+        ),
+        createAssociatedTokenAccountIdempotentInstruction(
+          program.provider.publicKey,
+          mintDetails.ata,
+          mintDetails.authority,
+          mintDetails.mint,
+          TOKEN_PROGRAM_ID
+        ),
+        createMintToInstruction(
+          mintDetails.mint,
+          mintDetails.ata,
+          mintDetails.authority,
+          1_000_000_000,
+          [],
+          TOKEN_PROGRAM_ID
+        ),
+      ]);
+
+      // Add all these instructions to our transaction
+      let tx = new Transaction();
+      tx.instructions = [
+        ...sendSolInstructions,
+        ...createMintInstructions,
+        ...mintTokensInstructions,
+      ];
+
+      const transactionSignature = await program.provider.sendAndConfirm(tx, [
+        tokenMintA,
+        tokenMintB,
+        alice,
+        bob,
+      ]);
+
+      // // Save the accounts for later use
+      // accounts.maker = alice.publicKey;
+      // accounts.taker = bob.publicKey;
+      // accounts.tokenMintA = tokenMintA.publicKey;
+      // accounts.makerTokenAccountA = aliceTokenAccountA;
+      // accounts.takerTokenAccountA = bobTokenAccountA;
+      // accounts.tokenMintB = tokenMintB.publicKey;
+      // accounts.makerTokenAccountB = aliceTokenAccountB;
+      // accounts.takerTokenAccountB = bobTokenAccountB;
     }
-  });
+  );
 
   it("Is initialized!", async () => {
     // Add your test here.
@@ -54,11 +168,11 @@ describe("escrow", () => {
       .makeOffer(new BN(0), new BN(100))
       .accounts({
         offer: offerAddress,
-        maker: maker.publicKey,
+        maker: alice.publicKey,
         tokenMintA,
         tokenMintB,
       })
-      .signers([maker])
+      .signers([alice])
       .rpc();
 
     // Fetch the created offer account
@@ -66,7 +180,7 @@ describe("escrow", () => {
 
     assert.isNotNull(createdOffer);
     assert.equal(createdOffer.tokenBWantedAmount.toNumber(), 100);
-    assert.equal(createdOffer.maker.toBase58(), maker.publicKey.toBase58());
+    assert.equal(createdOffer.maker.toBase58(), alice.publicKey.toBase58());
   });
 
   it("Can take an offer", async () => {
@@ -75,10 +189,10 @@ describe("escrow", () => {
       .takeOffer()
       .accounts({
         offer: offerAddress,
-        taker: taker.publicKey,
-        maker: maker.publicKey,
+        taker: bob.publicKey,
+        maker: alice.publicKey,
       })
-      .signers([taker])
+      .signers([bob])
       .rpc();
 
     const createdOffer = await program.account.offer.fetchNullable(
