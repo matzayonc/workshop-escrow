9b9c847af0546fcbca2e56f4d9f292e11c9ec5bc
Mateusz Zając <matzayonc@gmail.com>
2024-09-17 18:08:15 +0200

    token accounts

--- programs/escrow/src/instructions/take_offer.rs
+++ programs/escrow/src/instructions/take_offer.rs
@@ -1,4 +1,9 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, TokenAccount},
+    token_interface::TokenInterface,
+};
 
 use crate::Offer;
 
@@ -10,12 +15,46 @@ pub struct TakeOffer<'info> {
     #[account(mut)]
     pub maker: SystemAccount<'info>,
 
-    #[account(mut, close = maker,
-        has_one = maker,
-        seeds = [b"offer", maker.key().as_ref(), &offer.id.to_le_bytes()],
-        bump = offer.bump
-    )]
-    offer: Account<'info, Offer>,
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    // pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    // #[account(init_if_needed,
+    //     payer = taker,
+    //     associated_token::mint = token_mint_a,
+    //     associated_token::authority = taker,
+    //     associated_token::token_program = token_program
+    // )]
+    // pub taker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    // #[account(mut,
+    //     associated_token::mint = token_mint_b,
+    //     associated_token::authority = taker,
+    //     associated_token::token_program = token_program
+    // )]
+    // pub taker_token_account_b: InterfaceAccount<'info, TokenAccount>,
+
+    // #[account(mut,
+    //     associated_token::mint = token_mint_b,
+    //     associated_token::authority = maker,
+    //     associated_token::token_program = token_program
+    // )]
+    // pub maker_token_account_b: InterfaceAccount<'info, TokenAccount>,
+    // #[account(mut,
+    //     associated_token::mint = token_mint_a,
+    //     associated_token::authority = offer,
+    //     associated_token::token_program = token_program
+    // )]
+    // pub vault: InterfaceAccount<'info, TokenAccount>,
+    // #[account(mut, close = maker,
+    //     has_one = maker,
+    //     seeds = [b"offer", maker.key().as_ref(), &offer.id.to_le_bytes()],
+    //     bump = offer.bump
+    // )]
+    // offer: Account<'info, Offer>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
 }
 
 pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
